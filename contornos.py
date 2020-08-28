f14bdc55d50daaa94d198c5c16954b15861c9e5f
NightRoadIx <quintanarxbio@gmail.com>
2020-08-27 22:31:01 -0500

    Delete contornos.py

--- contornos.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-
-img = cv2.imread('bamel.jpg')
-imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,127,255,0)
-image, contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-img = cv2.drawContours(img, contours, -1, (0,255,0), 3)
