ab640d702e0b3e0c39972da3e6a74976ae51e348
NightRoadIx <quintanarxbio@gmail.com>
2020-10-07 01:31:08 -0500

    Update exempel009.py

--- exempel009.py
+++ exempel009.py
@@ -3,7 +3,25 @@ import numpy as np
 
 # Lectura de imágenes
 img1 = cv2.imread('mabel6.png')
+cv2.imshow('uno',img1)
+cv2.waitKey(0)
+
 img2 = cv2.imread('bamel.jpg')
+cv2.imshow('dos',img2)
+cv2.waitKey(0)
+
+# Negar la imagen mediante la operación lógica NOT
+'''
+    NOT |0|0|1|0|0|0|0|0| = |1|1|0|1|1|1|1|1|
+'''
+img1_inv = cv2.bitwise_not(img1)
+cv2.imshow('onu',img1_inv)
+cv2.waitKey(0)
+
+# Cambiar a escala de grises
+img2gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+cv2.imshow('gris',img2gray)
+cv2.waitKey(0)
 
 # Cambiar el peso de las imágenes
 dst = cv2.addWeighted(img1,0.7,img2,0.3,0)
