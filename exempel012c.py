5133762c726cd9e5754201c61c506a52118b8b28
NightRoadIx <quintanarxbio@gmail.com>
2023-09-11 15:34:18 -0600

    Update exempel012c.py

--- exempel012c.py
+++ exempel012c.py
@@ -52,6 +52,46 @@ ret,umbral = cv2.threshold(laBarata, 100, 255, cv2.THRESH_BINARY)
 cv2.imshow('Detector puntos umbralizado',laBarata)
 cv2.waitKey(0)
 
+## Más filtros
+# Estos son filtros de suavizado (Smoothing) que en general reducen
+# el ruido
+# Filtro Gaussiano
+# Basado en la función de distribución Gaussiana multivariante
+# z = f(x,y) = 1/(2 pi sigma^2)*exp(-(x^2 + y^2)/(2 sigma^2))
+# En teoría puede usarse un kernel de tamaño infinito, sin embargo,
+# se trunca en alededor de 3 desviaciones estándar (sigma)
+borroso = cv2.GaussianBlur(
+    gris, (9,9), 0
+)
+cv2.imshow('Gaussian',borroso)
+cv2.waitKey(0)
+# Umbralizado de la imagen obtenida
+ret,umbral = cv2.threshold(
+    borroso, 100, 255, cv2.THRESH_BINARY
+)
+cv2.imshow('Umbralizado',umbral)
+cv2.waitKey(0)
+
+# Filtro pa' hacer todo borroso
+# como cuando ya llevo 8 shots de tequila
+# Dr. Simi a filter2D
+# Filtro de suavizado, el cual resalta los bordes
+# quitando los detalles
+escandaloso = cv2.blur(
+    gris, (5,5)
+)
+cv2.imshow('Img tequila', escandaloso)
+cv2.waitKey(0)
+
+# Fitro mediana
+# Quita cambios abruptos de la imagen
+# alrededor del vecinario (kernel)
+mediana = cv2.medianBlur(
+    img2, 5
+)
+cv2.imshow('Hobbit', mediana)
+cv2.waitKey(0)
+
 cv2.destroyAllWindows()
 
 
