3046f99efe26e4d2ac41ae5195a5bfbe921b174a
NightRoadIx <quintanarxbio@gmail.com>
2023-12-05 17:31:44 -0600

    Update carajillo.py
    Se corrigio la forma de mostrar las imágeenes para observar el efecto de los filtros sobre las mismas

--- carajillo.py
+++ carajillo.py
@@ -28,14 +28,64 @@ def filtroIdealPB(D0, tam):
                 base[y, x] = 1
     return base
 
+def filtroGaussPB(D0, tam):
+    """
+    Imagen de un filtro pasabajas gaussiano con frecuencia de corte D0 y tamaño tam
+    :param D0: Frecuencia de corte
+    :param tam: Tamaño de la imagen a generar
+    :return: Imagen de un filtro pasabajas
+    """
+    base = np.zeros(tam[:2])
+    fil, col = tam[:2]
+    centro = (fil/2, col/2)
+    for x in range(col):
+        for y in range(fil):
+            base[y, x] = math.exp(((-distancia((y,x),centro)**2)/(2*(D0**2))))
+    return base
+
+def filtroIdealPA(D0, tam):
+    """
+    Imagen de un filtro pasaltas con frecuencia de corte D0 y tamaño tam
+    :param D0: Frecuencia de corte
+    :param tam: Tamaño de la imagen a generar
+    :return: Imagen de un filtro pasabajas
+    """
+    base = np.ones(tam[:2])
+    fil, col = tam[:2]
+    centro = (fil/2, col/2)
+    for x in range(col):
+        for y in range(fil):
+            if distancia((y, x), centro) < D0:
+                base[y, x] = 0
+    return base
+
+def filtroGaussPA(D0, tam):
+    """
+    Imagen de un filtro pasaltas gaussiano con frecuencia de corte D0 y tamaño tam
+    :param D0: Frecuencia de corte
+    :param tam: Tamaño de la imagen a generar
+    :return: Imagen de un filtro pasabajas
+    """
+    base = np.zeros(tam[:2])
+    fil, col = tam[:2]
+    centro = (fil/2, col/2)
+    for x in range(col):
+        for y in range(fil):
+            base[y, x] = 1 - math.exp(((-distancia((y,x),centro)**2)/(2*(D0**2))))
+    return base
+
 # Frecuencia de corte D0
-D0 = 500
+D0 = 10
 
 # ADQUISICIÓN DE IMÁGENES
 # Leer la imagen
 img = cv2.imread("gato.jpg")
 # Cambiamos a escala de grises
 gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+mp.subplot(3,3,1)
+mp.imshow(gris, cmap='gray')
+mp.title('Imagen original')
+mp.xticks([]), mp.yticks([])
 
 # TRANSFORMADA DE FOURIER
 # Una imagen en escala de grises es una señal
@@ -52,18 +102,20 @@ F = np.fft.fft2(gris)
 # a distinguir y así poder interpretar
 # Esto genera valores en decibeles
 M = 20*np.log(np.abs(F))
+mp.subplot(3,3,2)
 mp.imshow(M, cmap='gray')
+mp.title('Fourier de original')
 mp.xticks([]), mp.yticks([])
-mp.show()
 
 # Posteriormente, se modifica el espectro para mover
 # la frecuencia 0 al centro de la imagen
 # Se obtiene la transformada recorrida
 Fshift = np.fft.fftshift(F)
 Mshift = 20*np.log(np.abs(Fshift))
+mp.subplot(3,3,3)
 mp.imshow(Mshift, cmap='gray')
+mp.title('Fourier de original shifted')
 mp.xticks([]), mp.yticks([])
-mp.show()
 
 # FILTRADO
 # Generalmente, el filtrado se aplica con filtros
@@ -81,30 +133,34 @@ mp.show()
 
 # Filtro
 H = filtroIdealPB(D0, gris.shape)
-mp.imshow(H, cmap='gray')
+mp.subplot(3,3,6)
+mp.imshow(np.abs(H), cmap='gray')
+mp.title('Filtro fc=' + str(D0))
 mp.xticks([]), mp.yticks([])
-mp.show()
 
 # Multiplicar el filtro y la imagen para obtener la imagen filtrada
 # G(u,v) = F(u,v)H(u,v)
-# G = np.log(1+np.abs(Fshift*filtroIdealPB(100, gris.shape)))
 G = Fshift*H
-GM = np.log(20*np.abs(G)) #
+GM = np.log(1+np.abs(G))
+mp.subplot(3,3,9)
 mp.imshow(GM, cmap='gray')
+mp.title('Espectro imagen filtrada shifted')
 mp.xticks([]), mp.yticks([])
-mp.show()
 
 # Recorrer la imagen filtrada en frecuencia
 GH = np.fft.ifftshift(G)
-GHM = np.log(20*np.abs(GH)) #
+GHM = np.log(1+np.abs(GH))
+mp.subplot(3,3,8)
 mp.imshow(GHM, cmap='gray')
+mp.title('Espectro imagen filtrada')
 mp.xticks([]), mp.yticks([])
-mp.show()
 
 # Obtener la transformada inversa de Fourier
 g = np.fft.ifft2(GH)
 gm = np.log(20*np.abs(g))
+mp.subplot(3,3,7)
 mp.imshow(gm, cmap='gray')
+mp.title('Imagen filtrada')
 mp.xticks([]), mp.yticks([])
 mp.show()
 
